de08dbf69d2d19259f75c09229ae2763023ef51a
Dhanashri <dhanasp@thoughtworks.com>
2024-06-23 22:46:55 +0530

    Problem 2 - Arrays - isAnagram

---
+++ arrays/isAnagram.js
@@ -0,0 +1,44 @@
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+// An anagram is a string that contains the exact same characters as another string, 
+// but the order of the characters can be different.
+
+const isAnagram = (s,t) => {
+    if(s.length != t.length) return false
+    for(i=0; i< s.length; i++){
+        if(!t.includes(s[i])) return false
+        t = t.slice(0, t.indexOf(s[i])) + t.slice(t.indexOf(s[i])+1)
+    }
+    return true
+}
+
+timeComplexity = o(n^2)
+
+
+// take count of each key and compare
+// timeComplexity- o(n)
+const isAnagramA = (s,t) => {
+    if(s.length != t.length) return false
+
+    const countS = {}
+    const countT = {}
+
+    for(let i =0; i < s.length; i++){
+        countS[s[i]] = 1 + countS[s[i]] || 0
+        countT[t[i]] = 1 + countT[t[i]] || 0
+    }
+
+    for(const key in countS) {
+        if(countS[key] != countT[key]) return false 
+    }
+    return true
+
+}
+
+// sort and check
+// timeComplexity = o(nlogn) due to sorting
+const isAnagramS = (s,t) =>{
+    sortedS = s.split('').sort().join("")
+    sortedT = t.split('').sort().join("")
+    return sortedS === sortedT
+}
